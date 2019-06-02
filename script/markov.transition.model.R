dba5aba0166668c29a1b72ac349e8b848983f113
Deus Thindwa <dthindwa@imperial.ac.uk>
2019-06-02 09:50:53 +0100

    2June

--- script/markov.transition.model.R
+++ script/markov.transition.model.R
@@ -30,13 +30,3 @@ phirst$hivstatus <- if_else(phirst$age<1, rep(sample(c("unexposed","exposed"), s
 
 
 
-=======
-#written by Deus Thindwa
-#14/05/2019
-
-DDHP.packages <-c("foreign","tidyverse","janitor","readstata13","rstan","rethinking")
-lapply(DDHP.packages, library, character.only=TRUE)
-
-#load male questionnaire csv
-phirst.cohort <-as_tibble(read.dta13(""))
->>>>>>> 1b24a5c2f31a6acc6c545802cbb03a3f2c34869e

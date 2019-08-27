4b574defb1008222dcec25567fd6415c35f8c4c0
Deus Thindwa <dthindwa@imperial.ac.uk>
2019-08-27 08:27:49 +0100

    27/08/2019, GM

--- script/markov.transition.model.R
+++ script/markov.transition.model.R
@@ -1,93 +1,145 @@
 #Written by Deus Thindwa
 #Does living in a howusehold with HIV-infected individuals increase child risk of pneumococcal carriage acquisition?
 #An analysis using markov transition model, PhD chapter 1.
-#14/05/2019
+#13/08/2019
 
-#load required packages into memory
+#===============load required packages into memory===============
 phirst.packages <-c("tidyverse","plyr","msm","dplyr")
 lapply(phirst.packages, library, character.only=TRUE)
 
-#simulate a phirst dataset
+#===============simulate a phirst dataset===============
 set.seed(12345)
-sample.size=520
-phirst <- data.frame(hhid=rep(letters[1:26], each=20), #household id
-                     pid=rep.int(1:4, 5)) #participant id
-phirst$uid <- apply(phirst, 1 , function(x) paste0(toString(x[1]), toString(x[2]))) #household individual id
-phirst$sample.date <- seq(as.Date('2018/01/01'), as.Date('2019/06/04'), by="day") #sample date
-phirst <- ddply(phirst, .(uid), mutate, sample.point = seq_along(sample.date)) #sample point
-phirst <- ddply(phirst, .(uid), mutate, age=rep(rnorm(n=sample.size, mean=20, sd=9.5), each=5, length.out=5)) #age
-phirst$carrige <- sample(c("negative","positive"), size=sample.size, replace=TRUE) #carriage
-phirst$serotype <- if_else(phirst$carrige=="positive", sample(c("1","3","5","6A","6B","14","19A","19F","23F"), size=sample.size, replace=TRUE),
-                   if_else(phirst$carrige=="negative", NULL,NULL)) #serotype
-phirst$hivstatus <- if_else(phirst$age<1, rep(sample(c("unexposed","exposed"), size=sample.size, replace=TRUE), each=5, length.out=520),
-                           if_else(phirst$age>=1, rep(sample(c("negative","positive"), size=sample.size, replace=TRUE), each=5, length.out=520),NULL)) #hiv status
+sample.size=4862
 
-#analysis using "msm" R package
-#load coronary allograft vasculopathy dataset
-cav <- msm::cav
+#8 individuals per household (32=total FU-frequency, 8=no. of HH members, 4=individual FU-frequency)
+phirst1 <- data.frame(hid=rep(letters[1:26], each=32), pid=rep.int(1:8, 4))
+phirst1$sample.date <- seq(as.Date('2018/01/01'), as.Date('2020/04/11'), by="day")
+phirst1$prefix <- "a"
+phirst1$hhid <- apply(phirst1, 1 , function(x) paste0(toString(x[4]), toString(x[1])))
+phirst1$iid <- apply(phirst1, 1 , function(x) paste0(toString(x[5]), toString(x[2])))
+phirst1 <- subset(phirst1, select=c(hhid, iid, sample.date))
+phirst1 <- ddply(phirst1, .(iid), mutate, age=rep(abs(rnorm(n=sample.size, mean=20, sd=9.5)), each=4, length.out=4))
+phirst1$hiv <- if_else(phirst1$age<1,rep(sample(c("0","0"),size=832, replace=TRUE), each=4, length.out=832),
+               if_else(phirst1$age>=1,rep(sample(c("0","1"),size=832, replace=TRUE), each=4, length.out=832),NULL))
 
-#exclude pdiag missing obs
-cav <- cav[!is.na(cav$pdiag),]
+#4 individuals per household
+phirst2 <- data.frame(hid=rep(letters[1:26], each=20), pid=rep.int(1:4, 5))
+phirst2$sample.date <- seq(as.Date('2018/01/01'), as.Date('2019/06/04'), by="day")
+phirst2$prefix <- "b"
+phirst2$hhid <- apply(phirst2, 1 , function(x) paste0(toString(x[4]), toString(x[1])))
+phirst2$iid <- apply(phirst2, 1 , function(x) paste0(toString(x[5]), toString(x[2])))
+phirst2 <- subset(phirst2, select=c(hhid, iid, sample.date))
+phirst2 <- ddply(phirst2, .(iid), mutate, age=rep(abs(rnorm(n=sample.size, mean=20, sd=9.5)), each=5, length.out=5))
+phirst2$hiv <-0
 
-#summarise number of transitions between states
-cav[1:10,]
-statetable.msm(cav$state, cav$PTNUM)
+#5 individuals per household
+phirst3 <- data.frame(hid=rep(letters[1:26], each=30), pid=rep.int(1:5, 6))
+phirst3$sample.date <- seq(as.Date('2018/01/01'), as.Date('2020/02/19'), by="day")
+phirst3$prefix <- "c"
+phirst3$hhid <- apply(phirst3, 1 , function(x) paste0(toString(x[4]), toString(x[1])))
+phirst3$iid <- apply(phirst3, 1 , function(x) paste0(toString(x[5]), toString(x[2])))
+phirst3 <- subset(phirst3, select=c(hhid, iid, sample.date))
+phirst3 <- ddply(phirst3, .(iid), mutate, age=rep(abs(rnorm(n=sample.size, mean=20, sd=9.5)), each=6, length.out=6))
+phirst3$hiv <- if_else(phirst3$age<1,rep(sample(c("0","0"),size=780, replace=TRUE), each=6, length.out=780),
+               if_else(phirst3$age>=1,rep(sample(c("0","1"),size=780, replace=TRUE), each=6, length.out=780),NULL))
 
-#construct a transition intensity matrix ..Q..with initial diffuse priors
-transitionM <- rbind(c(0, 0.25, 0, 0.25), c(0.166, 0, 0.166, 0.166), c(0, 0.25, 0, 0.25), c(0, 0, 0, 0))
-rownames(transitionM) <- colnames(transitionM) <- c("Well", "Mild", "Severe", "Death")
+#15 individuals per household
+phirst4 <- data.frame(hid=rep(letters[1:26], each=105), pid=rep.int(1:15, 7))
+phirst4$sample.date <- seq(as.Date('2018/01/01'), as.Date('2025/06/22'), by="day")
+phirst4$prefix <- "d"
+phirst4$hhid <- apply(phirst4, 1 , function(x) paste0(toString(x[4]), toString(x[1])))
+phirst4$iid <- apply(phirst4, 1 , function(x) paste0(toString(x[5]), toString(x[2])))
+phirst4 <- subset(phirst4, select=c(hhid, iid, sample.date))
+phirst4 <- ddply(phirst4, .(iid), mutate, age=rep(abs(rnorm(n=sample.size, mean=20, sd=9.5)), each=7, length.out=7))
+phirst4$hiv <-0
 
-#calulate maximum likelihood estimates of the transition intensity matrix..Q
-transitionMLE <- msm(state ~ years, subject=PTNUM, data=cav, qmatrix=transitionM, death=4)
+#combine all datasets
+phirst <- rbind(phirst1, phirst2, phirst3, phirst4)
+remove(phirst1,phirst2,phirst3,phirst4)
 
-#display fitted transition probability matrix over specified time interval of 1 year..Q=Exp(tQ)
-pmatrix.msm(transitionMLE, t=1, ci="normal")
+#create sampling points for each individual
+phirst <- ddply(phirst, .(iid), mutate, sample.point = seq_along(sample.date))
 
-#fit transition intensity matrix..Q..with covariates
-cav$ihd <- as.numeric(cav[,"pdiag"]=="IHD")
-transitionMLE.cov <- msm(state ~ years, subject=PTNUM, data=cav, covariates=~dage+sex, qmatrix=transitionM,death=4,
-                         method="BFGS", control=list(fnscale=4000, maxit=10000))
+#define carriage state for each individuals
+phirst$state <- as.integer(sample(c("1","2"), size=sample.size, replace=TRUE))
 
-#obtain transition probability matrix at specified time interval of 1 year..Q=Exp(tQ)..by sex covariate
-pmatrix.msm(transitionMLE.cov, t=1, ci="normal", covariates=list(sex=0))
-pmatrix.msm(transitionMLE.cov, t=1, ci="normal", covariates=list(sex=1))
+#define missing state with 15% missing swabs
+phirst$state[sample(1:length(phirst$state),730)] <- NA
+phirst$missing <- if_else(is.na(phirst$state),1,0)
 
-#calculate hazard ratios (cov value comparison) of transitioning between states log scale
-hazard.msm(transitionMLE.cov)
+#define serotype carried by each individuals
+phirst$serotype <- if_else(phirst$state==2,sample(c("1","3","5","6A","6B","14","19A","19F","23F"),size=sample.size,replace=TRUE),
+                   if_else(phirst$state==1, NULL,NULL)) #serotype
+                   phirst$serotype[sample(1:length(phirst$serotype),1460)] <- NA
 
-#calculate transition intensity matrix of specified cov value
-qmatrix.msm(transitionMLE.cov, covariates=list(dage=50,ihd=1))
 
-#model comparisons
-lrtest.msm(transitionMLE,transitionMLE.cov)
+#phirst$Tobs <- c(1L, phirst$uid[-1]!=phirst$uid[-nrow(phirst)]) 
 
-#hidden Markov model
-fev <- msm::fev
-three.q<-rbind(c(0,exp(-6),exp(-9)), c(0,0,exp(-6)), c(0,0,0))
-hmodel1<-list(hmmNorm(mean=100, sd=16), hmmNorm(mean=54, sd=18), hmmIdent(999))
 
-fev1.msm<-msm(fev~days, subject=ptnum, data=fev, 
-              qmatrix=three.q, 
-              hmodel=hmodel1, 
-              hcovariates=list(~acute, ~acute, NULL),
-              hconstraint=list(acute=c(1,1)),death=3, method="BFGS")
+#===============hidden markov models using "msm" R package===============
 
-sojourn.msm(fev1.msm)
-viterbi.msm(fev1.msm)
+#---------------MODEL1---------------
+#show the state table
+statetable.msm(state,iid,data=phirst)
+                   
+#define initial values of a transition intensity matrix Q
+model1.Q <- rbind(c(0.5,0.5), c(0.5,0.5))
+rownames(model1.Q) <- c("Clear","Carrying")
+colnames(model1.Q) <- c("Clear","Carrying")
 
-#misclassification
-ematrix<- rbind(c(0,0.1,0,0), c(0.1,0,0.1,0), c(0,0.1,0,0), c(0,0,0,0))
-rownames(ematrix)<-colnames(ematrix)<-c("Well","Mild","Severe","Death")
-one4way<- rbind(c(0,0.1,0,0.04), c(0,0,0.3,0.05), c(0,0,0,0.3), c(0,0,0,0))
-rownames(one4way)<-colnames(one4way)<-c("Well","Mild","Severe","Death")
+#define initial values of an emission matrix E
+model1.E <- rbind(c(0.5,0.5), c(0,0.5))
+rownames(model1.E) <- c("SwabNeg","SwabPos")
+colnames(model1.E) <- c("Clear","Carrying")
 
-misc.msm<-msm(state~years, subject=PTNUM, data=cav,
-              qmatrix=one4way,
-              ematrix=ematrix,
-              obstrue=firstobs,
-              death=TRUE,
-              method="BFGS")
+#create an outcome distribution of each observed state (swabs)
+model1.HMM <- list(hmmBinom(1,0.5), hmmBinom(1,0.5))
 
+#fit a 2-state hidden markov model accounting for false negatives
+model1.msm <- msm(state~sample.point, subject=iid, data=phirst,
+                  qmatrix=model1.Q,
+                  ematrix=model1.E,
+                  hmodel=model1.HMM,
+                  method="BFGS")
+
+#maximum likelihood estimates of intensity matrix and outcome distributions
+printold.msm(model1.msm)
+
+#transition rates and 95%CI of HMM
+sojourn.msm(model1.msm)
+
+#most likely true series of states underlying the data
+viterbi.msm(model1.msm)
+
+#---------------MODEL2---------------
+#define initial values of a transition intensity matrix Q
+model2.Q <- rbind(c(0,0.3,0), c(0.05,0,0),c(0,0,0))
+rownames(model2.Q) <- c("Clear","Carrying","Missing")
+colnames(model2.Q) <- c("Clear","Carrying","Missing")
+
+#define initial values of an emission matrix E
+model2.E <- rbind(c(0,0.15,0), c(0,0,0), c(0.1,0.2,0))
+rownames(model2.E) <- c("SwabNeg","SwabPos","Missing")
+colnames(model2.E) <- c("Clear","Carrying","Missing")
+
+#create an outcome distribution of each observed state (swabs)
+model2.HMM <- list(hmmBinom(1,0.5), hmmBinom(1,0.5), hmmBinom(1,0.15))
+
+#fit a 2-state hidden markov model accounting for false negatives
+model2.msm <- msm(carrige~sample.point, subject=uid, data=phirst,
+                  qmatrix=model2.Q,
+                  ematrix=model2.E,
+                  hmodel=model2.HMM,
+                  method="BFGS")
+
+#maximum likelihood estimates of intensity matrix and outcome distributions
+printold.msm(model2.msm)
+
+#transition rates and 95%CI of HMM
+sojourn.msm(model2.msm)
+
+#most likely true series of states underlying the data
+viterbi.msm(model2.msm)
 
 
 

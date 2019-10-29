8465866ff1d1f2098e881f7f13117529bcf035f5
Deus <d.thindwa@imperial.ac.uk>
2019-10-29 18:54:33 +0000

    29/10/2019,LSHTM

--- script/markov.transition.model.R
+++ script/markov.transition.model.R
@@ -15,7 +15,8 @@ phirst$age <- if_else(phirst$age<40 & phirst$firstobs==1,0L,if_else(phirst$age>=
 phirst <- phirst%>%group_by(PTNUM)%>%fill(starts_with("age"), .direction="down")
 colnames(phirst) <- c("iid","wks","state","age","hiv","serotype","firstobs")
 phirst$hiv <- if_else(phirst$hiv=="IHD",1L,0L)
-phirst$state <- if_else(phirst$state==3,1L,if_else(phirst$state==4,4L,if_else(phirst$state==1,1L,2L)))
+#phirst <- subset(phirst, wks<=15)
+phirst$state <- if_else(phirst$state==3,2L,if_else(phirst$state==4,1L,if_else(phirst$state==1,1L,2L)))
 phirst$serotype <- if_else(phirst$state==2,sample(c("1","3","4","5","6A","6B","7F","9V","14","18C","19A","19F","23F"),size=2846,replace=TRUE),NULL) #serotype
 
 #---------------show transition frequency in state table
@@ -27,47 +28,27 @@ matrix.Q <- rbind(c(0.0,0.1),
 rownames(matrix.Q) <- c("Clear","Carrying")
 colnames(matrix.Q) <- c("Clear","Carrying")
 
-#---------------initiate emission matrix E
-matrix.E <- rbind(c(0.0,0.0), 
-                  c(0.1,0.0))
-colnames(matrix.E) <- c("SwabNeg","SwabPos")
-rownames(matrix.E) <- c("Clear","Carrying")
-
 #---------------fit a simpler model without misclassification
-p.model1<-msm(state~wks, subject=iid, data=phirst, qmatrix=matrix.Q, censor=4, gen.inits=TRUE, covariates=~age+hiv, control=list(trace=1,REPORT=1), cl=0.95)
+p.model1<-msm(state~wks, subject=iid, data=phirst, 
+              qmatrix=matrix.Q, 
+              #censor=c(3,4), censor.states=list(c(1,2),c(1,2)), 
+              covariates=~age+hiv,
+              pci=5,
+              control=list(trace=1,REPORT=1), cl=0.95)
 printnew.msm(p.model1)
 
-#---------------assessment of models with and without covariates
+#---------------assessment of a model without misclassification
 #(a) observed vs expected
-plot.prevalence.msm(p.model1, mintime=0, maxtime=15)
-phirst.prev <-tk_tbl(prevalence.msm(p.model1), preserve_index = TRUE, rename_index = "time") 
-phirst.prev <- subset(phirst.prev, select=c(time,Observed.percentages.State.1,Observed.percentages.State.2,Expected.percentages.Clear,Expected.percentages.Carrying))
-colnames(phirst.prev) <- c("time","obs.clear","obs.carry","exp.clear","exp.carry")
-
-
-A<-ggplot() +
-  geom_line(data=phirst.prev, aes(x=time, y=obs.clear), colour="darkgreen") +
-  geom_line(data=phirst.prev, aes(x=time, y=exp.clear), colour="darkgreen", lty="dashed") + 
-  scale_y_discrete(limits = c(0,20,40,60,80,100)) + 
-  scale_x_discrete(limits = c(0,5,10,15,20)) +
-  coord_cartesian(ylim=c(0,100)) + 
-  labs(title="Clear", x="time (weeks)", y="prevalence (%)") + 
-  theme_classic()
-
-B<-ggplot() +
-  geom_line(data=phirst.prev, aes(x=time, y=obs.carry), colour="darkred") +
-  geom_line(data=phirst.prev, aes(x=time, y=exp.carry), colour="darkred", lty="dashed") + 
-  scale_y_discrete(limits = c(0,20,40,60,80,100)) + 
-  scale_x_discrete(limits = c(0,5,10,15,20)) +
-  coord_cartesian(ylim=c(0,100)) + 
-  labs(title="Carrying", x="time (weeks)", y="prevalence (%)") + 
-  theme_classic()
-
-grid.arrange(grobs=list(A, B), ncol=2,nrow=2)
+dev.off()
+par(mgp=c(2,1,0),mar=c(6,4,2,2)+0.1)
+plot.prevalence.msm(p.model1, mintime=0, maxtime=15,legend.pos=c(0,100),lwd.obs=2,lwd.exp=2,xlab="Times (weeks)",ylab="Prevalence%")
 
 #(b) pearson-type good of fit
 options(digits=2)
 pearson.msm(p.model1, timegroups=2)
+
+#(c) likelihood surface
+surface.msm(p.model1,params=c(8,8),np=50,type="filled.contour")
 
 #---------------fit Markov model1 with covariates seperately
 qmatrix.msm(p.model1, covariates=list(hiv=0))
@@ -84,28 +65,35 @@ qmatrix.msm(p.model1, covariates=list(hiv=1, age=1))
 qmatrix.msm(p.model1, covariates=list(hiv=0, age=1))
 qmatrix.msm(p.model1, covariates=list(hiv=0, age=0))
 
+#---------------initiate emission matrix E
+matrix.E <- rbind(c(0.0,0.0), 
+                  c(0.1,0.0))
+colnames(matrix.E) <- c("SwabNeg","SwabPos")
+rownames(matrix.E) <- c("Clear","Carrying")
 
+#---------------fit a complex model with misclassification
+p.model2 <- msm(state~wks, subject=iid, data=phirst,
+                qmatrix=matrix.Q, 
+                ematrix=matrix.E,
+                covariates= ~age+hiv,
+                #censor=c(3,4), censor.states=list(c(1,2),c(1,2)),
+                est.initprobs=T,
+                opt.method="bobyqa")
 
-#create an outcome distribution of each observed state (swabs)
-model1.HMM <- list(hmmBinom(1,0.5), hmmBinom(2,0.5))
-model1.HMM <- list(hmmBinom(1,0.5), hmmBinom(1,0.5))
+printnew.msm(p.model2)
 
-#fit a 2-state hidden markov model accounting for false negatives
-model1.msm <- msm(state~sample.p, subject=iid, data=phirst,
-                  qmatrix=model1.Q,
-                  ematrix=model1.E,
-                  hmodel=model1.HMM,
-                  covariates= ~agecat+hiv,
-                  pci=4,
-                  est.initprobs=T,
-                  opt.method="bobyqa")
+#---------------assessment of a model without misclassification
+#(a) observed vs expected
+dev.off()
+par(mgp=c(2,1,0),mar=c(6,4,2,2)+0.1)
+plot.prevalence.msm(p.model2, mintime=0, maxtime=15,legend.pos=c(0,100),lwd.obs=2,lwd.exp=2,xlab="Times (weeks)",ylab="Prevalence%")
 
-#diagnosdtics plots
-plot.survfit.msm(model3.msm, main="model 3", mark.time=FALSE)
-plot.prevalence.msm(model3.msm)
+#(b) pearson-type good of fit
+options(digits=2)
+pearson.msm(p.model2, timegroups=2)
 
-
-
+#(c) likelihood surface
+surface.msm(p.model2,params=c(1,2),np=50,type="filled.contour")
 
 
 

1647acaa72e60ae59eb58bd6d144cb593c43992a
Deus Thindwa <deus.thindwa@gmail.com>
2020-06-15 01:17:41 +0100

    Model convergence & fitting

--- script/sfig1.R
+++ script/sfig1.R
@@ -1,7 +1,7 @@
 #Written by Deus Thindwa
 #Estimating the contribution of HIV-infected adults to household pneumococcal transmission in South Africa, 2016-2018.
 #Continuous-time time-homogeneous hidden Markov modelling study, PhD chapter 1.
-#20/9/2019 - 11/3/2020
+#20/9/2019 - 10/6/2020
 
 #model convergence
 phirst.cg <- read.csv("~/Rproject/Markov.Model.Resources/data/convergence.csv")
